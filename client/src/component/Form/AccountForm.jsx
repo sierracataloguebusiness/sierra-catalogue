9b7185abfb847025f1b603f3ef2175f49938df46
sierracataloguebusiness <sierracatalogue.business@gmail.com>
2025-10-04 16:49:47 +0000

    Update AccountForm.jsx

--- client/src/component/Form/AccountForm.jsx
+++ client/src/component/Form/AccountForm.jsx
@@ -146,7 +146,7 @@ const AccountForm = ({ onClose, isLoginForm, setIsLoginForm }) => {
 
     try {
       setLoading(true);
-      const response = await fetch("http://localhost:5000/api/auth/signup", {
+      const response = await fetch("https://sierra-catalogue.onrender.com/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(signupForm),
