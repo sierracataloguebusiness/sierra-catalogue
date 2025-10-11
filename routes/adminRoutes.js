f383d674bee210d826980049cc52fd31f30ae244
sierracataloguebusiness <sierracatalogue.business@gmail.com>
2025-10-11 20:25:58 +0000

    AdminPanel.jsx design backend 3

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import {getAdminStats} from "../controllers/AdminController.js";
 import {authorize} from "../middleware/roleMiddleware.js";
+import {protect} from "../middleware/authMiddleware.js";
 
-const router = express.Router();
+const router = express.Router(protect);
 
 router.get("/stats", authorize("admin"), getAdminStats);
 
