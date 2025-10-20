cbefaf2c7fbe9c5e707324407541af87a862d6df
sierracataloguebusiness <sierracatalogue.business@gmail.com>
2025-10-20 13:33:42 +0000

    VendorProduct

--- client/src/pages/dashboard/vendor/VendorProduct.jsx
+++ client/src/pages/dashboard/vendor/VendorProduct.jsx
@@ -4,6 +4,10 @@ import FormInput from "../../../component/Form/FormComponents/FormInput.jsx";
 const VendorProduct = () => {
   const [form, setForm] = useState({
     title: "",
+    price: "",
+    stock: 1,
+    category: "",
+    image: "",
   });
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -25,12 +29,38 @@ const VendorProduct = () => {
             value={form.title}
           />
 
-          <select>
-            <option value="">Category 1</option>
-            <option value="">Category 2</option>
-            <option value="">Category 3</option>
-            <option value="">Category 4</option>
+          <select
+            value={form.category}
+            defaultValue=""
+            className="bg-gray-800 text-white px-2 py-1 rounded-md"
+          >
+            <option value="" disabled>
+              Select Category
+            </option>
+            <option value="customer">Category 1</option>
+            <option value="vendor">Category 2</option>
+            <option value="admin">Category 3</option>
           </select>
+
+          <FormInput
+            placeholder="Price"
+            hasLabel={false}
+            onChange={handleChange}
+            hasError={false}
+            name="price"
+            value={form.price}
+          />
+
+          <FormInput
+            placeholder="Stock"
+            hasLabel={false}
+            onChange={handleChange}
+            hasError={false}
+            name="stock"
+            value={form.stock}
+          />
+
+          <input type={"image"} value={form.image} />
         </form>
       </div>
       {/*  edit products  */}
